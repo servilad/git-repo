fbdf97d99ff7d8b6bc2818f934e703ffb8af7de0
servilad <servilad62@yandex.ru>
2019-09-10 17:50:52 +0300

    Добавена строчка

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Goodby");
             Console.ReadKey();
         }
     }
